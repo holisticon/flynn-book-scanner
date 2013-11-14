36532543464dfd0c3bd281432d5063aac2f8c4cf
Daniel Wegener <daniel.wegener@holisticon.de>
2013-11-14 18:45:40 +0100

    + enhanced bookView

--- app/scripts/controllers/bookController.js
+++ app/scripts/controllers/bookController.js
@@ -10,8 +10,8 @@
             var search = $location.search().isbn;
             $scope.isbn = search;
 
-            var bookResource = $http.get("https://www.googleapis.com/books/v1/volumes/?q=:isbn="+$scope.isbn+"&projection=full").success(function(data) {
-                $scope.book = data.items[0];
+            var bookResource = $http.get("https://www.googleapis.com/books/v1/volumes/?q=:isbn="+$scope.isbn+"&projection=full&maxResults=1").success(function(data) {
+                $scope.books = data.items;
             });
 
         }]);
