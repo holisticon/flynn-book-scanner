58141e2dec7c03fbe1a292e6131c70b41bf312da
Oliver Ochs <oliver.ochs@holisticon.de>
2014-01-17 20:53:51 +0100

    removed builded files

--- platforms/android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-/Users/simonox/js-workshop/flynn-book-scanner/platforms/android/gen/de/holisticon/flynn/R.java \
- : /Users/simonox/js-workshop/flynn-book-scanner/platforms/android/res/drawable/icon.png \
-/Users/simonox/js-workshop/flynn-book-scanner/platforms/android/res/drawable-hdpi/icon.png \
-/Users/simonox/js-workshop/flynn-book-scanner/platforms/android/res/drawable-ldpi/icon.png \
-/Users/simonox/js-workshop/flynn-book-scanner/platforms/android/res/drawable-mdpi/icon.png \
-/Users/simonox/js-workshop/flynn-book-scanner/platforms/android/res/drawable-xhdpi/icon.png \
-/Users/simonox/js-workshop/flynn-book-scanner/platforms/android/res/values/strings.xml \
-/Users/simonox/js-workshop/flynn-book-scanner/platforms/android/res/xml/config.xml \
-/Users/simonox/js-workshop/flynn-book-scanner/platforms/android/bin/AndroidManifest.xml \
