59db9dd04fcde203476be19838b6e11bf2a0c643
Oliver Ochs <oliver.ochs@holisticon.de>
2014-01-17 21:29:32 +0100

    display found book

--- app/scripts/controllers/bookController.js
+++ app/scripts/controllers/bookController.js
@@ -10,12 +10,20 @@
                     function (result) {
                         alert('We got a barcode\n' +
                             'Result: ' + result.text + '\n' +  'Format: ' + result.format + '\n' +  'Cancelled: ' + result.cancelled);
+                        retrieve(result.text);
                     },
                     function (error) {
                         alert('Scanning failed: ' + error);
                     }
                 );
             }
+
+            function retrieve(isbn) {
+                $http.get('https://www.googleapis.com/books/v1/volumes/?q=:isbn='+isbn+'&projection=full&maxResults=1').success(function(data) {
+                    $scope.books = data.items;
+                });
+            }
+
             // https://www.googleapis.com/books/v1/volumes/?q=:isbn=9781849682398
             $scope.isbn = $location.search().isbn;
 
