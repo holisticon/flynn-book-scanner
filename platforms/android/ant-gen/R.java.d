2c65af54ece7b547928bec3ace9879c8529d86cf
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-06-10 15:00:44 +0200

    update android platform

---
+++ platforms/android/ant-gen/R.java.d
@@ -0,0 +1,17 @@
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/ant-gen/de/holisticon/flynn/R.java \
+ : /Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable/icon.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable-hdpi/icon.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable-land-hdpi/screen.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable-land-ldpi/screen.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable-land-mdpi/screen.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable-land-xhdpi/screen.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable-ldpi/icon.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable-mdpi/icon.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable-port-hdpi/screen.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable-port-ldpi/screen.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable-port-mdpi/screen.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable-port-xhdpi/screen.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/values/strings.xml \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/res/xml/config.xml \
+/Users/mreinhardt/Documents/Programmierung/repositories/git/flynn-book-scanner/platforms/android/ant-build/AndroidManifest.xml \
