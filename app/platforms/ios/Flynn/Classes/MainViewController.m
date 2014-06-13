c00c9dfde04aed2629821c8984d143233ababf94
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-06-13 10:01:16 +0200

    holistify app
    improve UI

--- app/platforms/ios/Flynn/Classes/MainViewController.m
+++ app/platforms/ios/Flynn/Classes/MainViewController.m
@@ -129,6 +129,20 @@
 }
 */
 
+- (BOOL)webView:(UIWebView *)theWebView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    NSURL *url = [request URL];
+    
+    // Intercept the external http requests and forward to Safari.app
+    // Otherwise forward to the PhoneGap WebView
+    if ([[url scheme] isEqualToString:@"http"] || [[url scheme] isEqualToString:@"https"]) {
+        [[UIApplication sharedApplication] openURL:url];
+        return NO;
+    }
+    else {
+        return [ super webView:theWebView shouldStartLoadWithRequest:request navigationType:navigationType ];
+    }
+}
 @end
 
 @implementation MainCommandDelegate
