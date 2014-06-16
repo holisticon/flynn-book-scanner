eaea7dc32b8ff90ad88a25e774e5e766ed309702
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-06-16 20:13:46 +0200

    Adding controller test
    see #3

---
+++ app/test/spec/controllers/mainSpec.js
@@ -0,0 +1,81 @@
+var cordova;
+
+describe('main', function() {
+
+  // load the controller's module
+  beforeEach(module('flynnBookScannerApp'));
+
+  describe("SearchController", function() {
+
+    var fakeFactory, deferred, q,
+      controller,
+      bookService,
+      scope,
+      httpBackend,
+      settings;
+
+    // define the mock book service
+    beforeEach(function() {
+      inject(function(GoogleBookService, $q, $rootScope) {
+        q = $q;
+        deferred = $q.defer();
+        scope = $rootScope;
+        bookService = GoogleBookService;
+        var books = [{
+          "kind": "books#volumes",
+          "totalItems": 77,
+          "items": [{
+            "kind": "books#volume",
+            "id": "lwz8ZwEACAAJ",
+            "etag": "b3Rk7DgfRR8",
+            "selfLink": "https://www.googleapis.com/books/v1/volumes/lwz8ZwEACAAJ",
+            "volumeInfo": {
+              "title": "EJB 3.1 professionell",
+              "subtitle": "Grundlagen- und Expertenwissen zu Enterprise JavaBeans 3.1 - inkl. JPA 2.0",
+              "publishedDate": "2011",
+              "industryIdentifiers": [{
+                "type": "ISBN_10",
+                "identifier": "3898646122"
+              }, {
+                "type": "ISBN_13",
+                "identifier": "9783898646123"
+              }],
+            }
+          }]
+        }];
+        var response = {};
+        response.books = books;
+        deferred.resolve(response);
+        spyOn(bookService, 'search').andReturn(deferred.promise);
+      });
+    });
+
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function($rootScope, $controller, GoogleBookService, $q) {
+      scope = $rootScope.$new();
+      q = $q;
+      controller = $controller("SearchController", {
+        $scope: scope,
+        GoogleBookService: bookService
+      });
+    }));
+
+    it('Init module failed', function() {
+      expect(true).toBe(true);
+    });
+
+    it('do empty search', function() {
+      scope.searchQuery = null;
+      scope.search();
+    });
+
+
+    it('do isbn search', function() {
+      scope.searchQuery = {};
+      scope.searchQuery.isbn = "9783898646123";
+      scope.search();
+      scope.$apply();
+      expect(scope.books.length).toEqual(1);
+    });
+  });
+});
