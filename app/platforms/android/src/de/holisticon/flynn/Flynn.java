101a53245180f33b4b1ab9da41d85e9e580c5480
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-06-23 10:35:01 +0200

    Improve layout
    disable landscape on phones, see 10

--- app/platforms/android/src/de/holisticon/flynn/Flynn.java
+++ app/platforms/android/src/de/holisticon/flynn/Flynn.java
@@ -19,19 +19,38 @@
 
 package de.holisticon.flynn;
 
+import org.apache.cordova.Config;
+import org.apache.cordova.CordovaActivity;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import org.apache.cordova.*;
 
-public class Flynn extends CordovaActivity 
-{
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        super.init();
-        // Set by <content src="index.html" /> in config.xml
-        super.loadUrl(Config.getStartUrl());
-        //super.loadUrl("file:///android_asset/www/index.html");
-    }
+/**
+ * Flynn android app
+ * 
+ * @author mreinhardt
+ * 
+ */
+public class Flynn extends CordovaActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.init();
+		// Set by <content src="index.html" /> in config.xml
+		super.loadUrl(Config.getStartUrl());
+		Context ctx = super.getApplication().getBaseContext();
+		if (ctx != null && !isTablet(ctx)) {
+			// force portrait mode in small devices
+			super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		}
+		// super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+	public boolean isTablet(Context context) {
+		boolean xlarge = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == 4);
+		boolean large = ((context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE);
+		return (xlarge || large);
+	}
 }
-
