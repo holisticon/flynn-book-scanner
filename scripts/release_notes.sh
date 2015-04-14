aad1dc1907775b6767d1d2d59fd5889bcb6e57e4
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-04-14 21:20:29 +0200

    Fix typo error

--- scripts/release_notes.sh
+++ scripts/release_notes.sh
@@ -9,9 +9,10 @@ versionNumber=`grep "id=\"de.holisticon.app.flynn\" version" ${BASEDIR}/../app/c
 sed -e "s/version=\".*\"/version=\"${versionNumber}\" android-versionCode=\"${buildNo}\" ios-CFBundleVersion=\"${buildNo}\"/g" ${BASEDIR}/../app/config.xml > ${BASEDIR}/../app/config.xml.new
 mv ${BASEDIR}/../app/config.xml.new ${BASEDIR}/../app/config.xml
 
-git log `git describe --tags --abbrev=0`..HEAD --oneline >  ${BASEDIR}/../app/src-gen/RELEASE_NOTES
+git log `git describe --tags --abbrev=0`..HEAD --oneline  | sed 's/"//g' >  ${BASEDIR}/../app/src-gen/RELEASE_NOTES
+releaseNotes_="$(perl -p -e 's/\n/<br>/'  ${BASEDIR}/../app/src-gen/RELEASE_NOTES)"
 releaseNotes="$(perl -p -e 's/\n/<br>/'  ${BASEDIR}/../app/src-gen/RELEASE_NOTES)"
 
-# updat config.json
+# update config.json
 versionedConfig="$(jq  .info.version.value=.info.version.value+\""build${buildNo}"\" ${BASEDIR}/../app/src/config.json)"
 echo $versionedConfig | jq  .info.release_notes.value=\""${releaseNotes}"\" > ${BASEDIR}/../app/src-gen/config.json
