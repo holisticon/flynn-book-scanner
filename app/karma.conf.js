2cf05d5563882ee947df504d5a51e012c6393113
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-05-10 12:12:12 +0200

    use phantomJS for testing

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -79,7 +79,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS2'],
+    browsers: ['PhantomJS'],
 
     reporters: ['progress', 'junit'],
 
