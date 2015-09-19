b003b4b485e2e0d4e2664f3cfd154e6df3c5fc4b
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-09-19 13:29:28 +0200

    Update karma config

--- app/karma.conf.js
+++ app/karma.conf.js
@@ -1,75 +1,56 @@
 // Karma configuration
-// http://karma-runner.github.io/0.10/config/configuration-file.html
+// http://karma-runner.github.io/0.12/config/configuration-file.html
+// Generated on 2015-06-18 using
+// generator-karma 1.0.0
 
-module.exports = function(config) {
+module.exports = function (config) {
+  'use strict';
+
   config.set({
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
     // base path, that will be used to resolve files and exclude
-    basePath: '',
+    basePath: '.',
 
     // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+    // as well as any additional frameworks (requirejs/chai/sinon/...)
+    frameworks: [
+      'jasmine'
+    ],
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/jasmine-expect/dist/jasmine-matchers.js',
+      // bower:js
+      'bower_components/es5-shim/es5-shim.js',
       'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-cache/dist/angular-cache.min.js',
-      'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-cookies/angular-cookies.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-cookies/angular-cookies.js',
+      'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/angular-cache/dist/angular-cache.js',
       'bower_components/angular-local-storage/dist/angular-local-storage.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/ionic/release/js/ionic.js',
       'bower_components/ionic/release/js/ionic-angular.js',
-      'bower_components/angular-bootstrap/ui-bootstrap.js',
-      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-      'bower_components/es5-shim/es5-shim.js',
       'bower_components/pouchdb/dist/pouchdb.js',
-      'bower_components/pouchdb/dist/pouchdb.idb-alt.js',
-      'bower_components/pouchdb/dist/pouchdb.localstorage.js',
-      'bower_components/pouchdb/dist/pouchdb.memory.js',
-      'bower_components/pouchdb-find/dist/pouchdb.find.js',
-      'src/scripts/app.js',
-      'src/scripts/plugins/*.js',
-      'src/scripts/plugins/*.js',
-      'src/scripts/modules/*.js',
-      'src/scripts/controllers/*.js',
-      'test/spec/controllers/*.js',
-      'test/spec/services/*.js',
-      // fixtures
-      {
-        pattern: 'test/*.json',
-        watched: true,
-        served: true,
-        included: false
-      },
-      // html
-      {
-        pattern: 'src/views/*.html',
-        watched: true,
-        served: true,
-        included: true
-      }
+      'bower_components/pouchdb-find/dist/pouchdb.find.min.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-scenario/angular-scenario.js',
+      // endbower
+      "src/scripts/**/*.js",
+      "test/mocks/**/*.js",
+      "test/spec/**/*.js"
     ],
 
     // list of files / patterns to exclude
     exclude: [],
 
     // web server port
-    port: 8080,
-
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
+    port: 9080,
 
     // Start these browsers, currently available:
     // - Chrome
@@ -79,23 +60,36 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    browsers: [
+      'PhantomJS'
+    ],
 
-    reporters: ['progress', 'junit'],
+    reporters: [
+      'progress',
+      'junit',
+      'html'
+    ],
 
-    // the default configuration
     junitReporter: {
-      outputFile: 'target/test-results.xml',
-      suite: ''
+      outputDir: 'target/unit-tests/', // results will be saved as $outputDir/$browserName.xml
+      suite: 'flynnApp'
     },
-
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: false,
+
+    colors: true,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // Uncomment the following lines if you are using grunt's server to run the tests
+    // proxies: {
+    //   '/': 'http://localhost:9000/'
+    // },
+    // URL root prevent conflicts with the site root
+    // urlRoot: '_karma_'
   });
-  config.proxies = {
-    '/config.json': 'http://localhost:' + config.port + '/base/test/config.json',
-    '/views/': 'http://localhost:' + config.port + '/base/src/views/'
-  }
 };
