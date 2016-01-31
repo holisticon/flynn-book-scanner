90d210e9bf3f57baae19722fefc71bf31dbd2417
Martin Reinhardt <contact@martinreinhardt-online.de>
2016-01-31 11:38:46 +0100

    Fix for CI android build

---
+++ scripts/setupAndroidSDK.sh
@@ -0,0 +1,15 @@
+echo ""
+echo "# Executing https://raw.github.com/embarkmobile/android-sdk-installer"
+echo ""
+sudo apt-get install -qq libstdc++6:i386 lib32z1
+curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer \
+  | bash /dev/stdin --install=build-tools-22.0.1,android-22,sys-img-armeabi-v7a-android-22 \
+  && source ~/.android-sdk-installer/env
+
+echo ""
+echo "# Creating AVD"
+echo ""
+
+echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
+emulator -avd test -no-skin -no-audio -no-window &
+#wait_for_emulator
